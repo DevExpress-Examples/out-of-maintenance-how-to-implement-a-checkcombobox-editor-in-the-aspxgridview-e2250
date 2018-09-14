38d45258c3876f7bbfe0a06034dee3502456dc8f
LexDX <fredom100000@yandex.ru>
2018-09-14 08:50:32 +0300

    fix web.config for new versions and line endings

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -16,23 +16,23 @@ public partial class _Default : System.Web.UI.Page {
         set {
             HttpContext.Current.Session["gridData"] = value;
         }
-    }    
+    }
     protected void Page_Init(object sender, EventArgs e) {
         grid.DataSource = dataSource;
         grid.DataBind();
     }
 
-    protected void grid1_RowUpdating(object sender, ASPxDataUpdatingEventArgs e) {        
+    protected void grid1_RowUpdating(object sender, ASPxDataUpdatingEventArgs e) {
         GridDataHelper.UpdateRow(e.Keys, e.NewValues, dataSource);
-        CancelEdit(sender as ASPxGridView, e);        
+        CancelEdit(sender as ASPxGridView, e);
     }
 
-    protected void grid_RowDeleting(object sender, ASPxDataDeletingEventArgs e) {        
+    protected void grid_RowDeleting(object sender, ASPxDataDeletingEventArgs e) {
         GridDataHelper.DeleteRow(e.Keys, dataSource);
         CancelEdit(sender as ASPxGridView, e);
     }
 
-    protected void grid_RowInserting(object sender, ASPxDataInsertingEventArgs e) {        
+    protected void grid_RowInserting(object sender, ASPxDataInsertingEventArgs e) {
         GridDataHelper.InsertRow(e.NewValues, dataSource);
         CancelEdit(sender as ASPxGridView, e);
     }
